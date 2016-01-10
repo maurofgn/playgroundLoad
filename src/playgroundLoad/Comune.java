e2e569057c728fc571b573335c48b5dfa654088a
Mauro Fugante <mauro.fugante@gmail.com>
2016-01-10 15:02:15 +0100

    aggiunto altri comuni per aumentare la casistica

--- src/playgroundLoad/Comune.java
+++ src/playgroundLoad/Comune.java
@@ -19,4 +19,40 @@ public class Comune {
 		return nome;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codReg == null) ? 0 : codReg.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Comune other = (Comune) obj;
+		if (codReg == null) {
+			if (other.codReg != null)
+				return false;
+		} else if (!codReg.equals(other.codReg))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Comune [codReg=" + codReg + ", nome=" + nome + "]";
+	}
+
 }
